1667595a392782199eed3ee350440ffcf62e115f
Satvik <sharmasatvik43@gmail.com>
2025-01-12 20:43:34 +0530

    Add example of defining variables and constants in Python

---
+++ 01-variables-constants/var-and-const.py
@@ -0,0 +1,12 @@
+# This is the good practice how can we define variables and constants in python
+
+# Define constant values at top
+PI = 3.14
+
+
+# Define variables
+radius = 4
+
+area = PI * (radius ** 2)
+
+print("Area of the circle is: ", area)
