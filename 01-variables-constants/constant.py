cccf41fd578ccc74d8a711846c5fee6bace03eb2
Satvik <sharmasatvik43@gmail.com>
2025-01-12 20:41:09 +0530

    Add constants example in constant.py with explanations

---
+++ 01-variables-constants/constant.py
@@ -0,0 +1,10 @@
+# A constant is a name used to store data that remains unchanged throughout the program.
+# Python doesn't have built-in support for constants like some other languages (e.g., final in Java)
+
+# Constants are written in uppercase letters. They are not enforced by the language; it’s up to the programmer to avoid modifying them.
+
+# Syntax: CONSTANT_NAME = value
+
+PI = 3.14
+GRAVITY = 9.8
+AGE_OF_UNIVERSE = 13.8e9
