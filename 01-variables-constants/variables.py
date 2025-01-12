cbb122b33794d7cf7f9e8d927f1636274c1add71
Satvik <sharmasatvik43@gmail.com>
2025-01-12 20:38:42 +0530

    Add initial Python variable examples and a basic Hello World program

---
+++ 01-variables-constants/variables.py
@@ -0,0 +1,20 @@
+# Variable is the name that is used to store the data that can be changed during the execution of the program.
+
+# Since python is a dynamically typed language, so we don't need to specify the data type of the variable.
+
+# Syntax: variable_name = value
+
+age = 21
+name = "Satvik Sharma"
+height = 6.1
+isMale = True
+
+
+# Reassigning the values of the variable are also possible!
+age = 22
+
+# we can assign the different datatype values to the same variable.
+age = "Twenty Two"
+
+# we can also assign multiple values to multiple variables in a single line.
+a, b, c = 1, 2, 3
