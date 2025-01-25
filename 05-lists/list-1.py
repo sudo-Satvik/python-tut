bb9394fa1930133108c59c8316361b132f784b93
Satvik <sharmasatvik43@gmail.com>
2025-01-25 09:25:03 +0530

    Add examples of Python lists with diverse data types and print statements

---
+++ 05-lists/list-1.py
@@ -0,0 +1,7 @@
+# Python lists are containers to store a set of values of any data type.
+
+first_list_example = ["apple", "Satvik", 100, True, 0.2341]
+print(first_list_example)
+
+fruits_list = ["mango", "apple", "grapes", "guava", "pineapple"]
+print(fruits_list)
