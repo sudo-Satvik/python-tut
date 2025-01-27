da424db386cf8a197539a1e018f5d50de15a6d20
Satvik <sharmasatvik43@gmail.com>
2025-01-27 22:56:55 +0530

    Add examples and explanations for sets in Python, including creation, operations, and methods

---
+++ 08-sets/set-1.py
@@ -0,0 +1,15 @@
+# Set is the collection of non-repeatitive elements.
+# Set is unordered which means element order doesn't matter.
+# Elements in set can't be accessed by the indices.
+# Set is immutable.
+
+# Creating a set
+s = set()
+print(s)
+s.add(1)
+print(s)
+
+
+# Creating a set with elements
+s = {1, 2, 3, 4, 5}
+print(s)
---
+++ 08-sets/set-2.py
@@ -0,0 +1,31 @@
+# Opeations on sets
+
+set = {1, 8, 2, 3}
+print(set)
+
+# Length of the set ~ use len()
+print("length of the set: ",len(set))
+
+# Adding an element to the set ~ use add()
+# set.add(4)
+# print(set)
+
+# Removing an element from the set ~ use remove()
+# set.remove(8)
+# print(set)
+
+# Pop an element from the set and return it ~ use pop()
+# set.pop()
+# print(set)
+
+# Empty the set ~ use clear()
+# set.clear()
+# print(set)
+
+
+# Union of two sets ~ use union()
+print("Union of two sets: ", set.union({4, 5, 6}))
+
+
+# Intersection of two sets ~ use intersection()
+print("Intersection of two sets: ", set.intersection({2, 3, 4}))
