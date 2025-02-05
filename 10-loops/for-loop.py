0f4c1e4c032858b4b8e7bfd50b48e4c872ac4732
Satvik <sharmasatvik43@gmail.com>
2025-02-05 06:14:12 +0530

    Add examples and explanations for for loops in Python, including usage with lists, tuples, strings, and the range function

---
+++ 10-loops/for-loop.py
@@ -0,0 +1,39 @@
+# for loop is used to iterate among the list, tuple or strings[iterables]
+
+# list1 = [1, 2, 3, 4, 5]
+# for item in list1:
+#     print(item)
+
+# print("=============")
+
+# tuple1 = (1, 5, 2, 4, 3)
+# for element in tuple1:
+#     print(element)
+
+# print("=============")
+# string1 = "Satvik"
+
+# for char in string1:
+#     print(char)
+
+# print("=============")
+
+# The range() function in python is used to generate a sequence of number.
+# range(start, stop, step_size)
+
+# for i in range(6, 0, -1):
+#     print(i)
+
+
+
+# FOR LOOP WITH ELSE
+# An optional else can be used with a for loop if the code is to be executed when the
+# loops exhausts.
+
+for i in range(0, 11):
+    print(i)
+else:
+    print("loop ended")
+
+    
+# Exists: break, continue and pass (pass: do nothing)
